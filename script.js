1ad36adf686774c78d3be5d06ced0ec759a3092c
imozzuhin88-star <imozzuhin88@gmail.com>
2026-04-23 19:17:32 +0300

    added header

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let container = document.querySelector('.container');
+let cards = document.querySelector('.cards');
 
+let pics = ['Header_1jpg.', 'images.jpg',"images(1).jpg"];
+let texts = ['untitled', 'untitled'];
+
+for (let i = 0; i < pics.length; i += 1) {
+  let img = document.createElement('img');
+  img.src = pics[i];
+
+  let text = document.createElement('p');
+  img.className = 'text';
+  text.innerHTML = texts[i];
+
+  let card = document.createElement('div');
+  card.className = 'card';
+
+  card.appendChild(img);
+  card.appendChild(text);
+
+  cards.appendChild(card);
+}
